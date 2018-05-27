5a34afd8be3a56f66aa0734d1cdcb3ad54f46752
Hafeez <hafeez@ceribs.com>
2018-05-27 16:59:43 +0530

    Move all messages into seperate file

---
+++ server/const/messages.js
@@ -0,0 +1,11 @@
+module.exports = {
+  error: {
+    USER_ALREADY_EXISTS: 'User already exists!...',
+    INVALID_EMAIL_OR_PASSWORD: 'Invalid email or password!...',
+    INVALID_EMAIL: 'Invalid email!...',
+    INVALID_PASSWORD: 'Invalid password!...',
+  },
+  success: {
+    LOGIN_SUCCESS: 'Login success!...'
+  },
+};
