347c7daad162c7fb7d1fd9b339690b623c7c47b6
Hafeez <hafeez@ceribs.com>
2018-05-27 12:33:54 +0530

    optimization

--- server/controllers/api/v1/authController.js
+++ server/controllers/api/v1/authController.js
@@ -6,13 +6,13 @@ export const register = (req, res) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-  }).then(user => {
-    const { name, email } = user;
+  }).then(({ name, email }) => {
     res.json({ name, email });
   }).catch(error => {
     if (error.code === errorCodes.mongo.DUPLICATE) {
       res.status(errorCodes.http.CONFLICT).json({ message: 'User already exist!...' });
+    } else {
+      res.status(errorCodes.http.BAD_REQUEST).json(error);
     }
-    res.status(errorCodes.http.BAD_REQUEST).json(error);
   });
 };
