1c3cbe3ec135c1791ce5a74182509d58c7784a42
Hafeez <hafeez@ceribs.com>
2018-05-30 21:41:54 +0530

    Redux initialized

--- client/index.js
+++ client/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Routes from './routes';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-render(<Routes />, document.getElementById('app'));
+import Routes from './routes';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('app')
+);
